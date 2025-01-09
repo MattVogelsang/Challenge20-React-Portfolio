4a33c0c9334d6b9ffe75489113abe1154ae5f798
Mattvogelsang <vogelsangmatt@gmail.com>
2025-01-09 18:43:08 -0500

    savede

---
+++ src/pages/resume.jsx
@@ -0,0 +1,19 @@
+
+
+function Resume(){
+    return <>
+         <div id="about" class="parallax-window" data-parallax="scroll" data-image-src="img/antique-cafe-bg-03.jpg">
+        <div class="container mx-auto tm-container py-24 sm:py-48">
+            <div class="tm-item-container sm:ml-auto sm:mr-12 mx-auto sm:px-0 px-4">
+                <div class="bg-white bg-opacity-80 p-12 pb-14 rounded-xl mb-5">
+                    <h2 class="mb-6 tm-text-green text-4xl font-medium"><a href="/resume.pdf">Download Resume</a> </h2>
+                   
+                </div>
+                
+            </div>           
+        </div>        
+    </div>
+    </>
+}
+
+export default Resume
